5bc4b3a36ab73d3fcb4dc1f8832ca6a97c298a6a
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-02-02 18:36:13 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+function writeCards(names, event){
+    const newArray = [];
+    for(let i =0; i < names.length; i++){
+    newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    } 
+   
+   return newArray;
+};
+
+
+function countDown(count){
+
+    while (count >= 0) {
+        console.log(count);
+        count = count - 1;
+    }
+
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+countDown(10);
